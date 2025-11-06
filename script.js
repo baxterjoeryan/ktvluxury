d6ff87c0902db10d234e94d6f0be09aaf279858c
baxterjoeryan <baxterjoeryan@gmail.com>
2025-11-06 21:47:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -114,3 +114,26 @@ document.getElementById('contact-form').addEventListener('submit', function(e){
 
 // initialize default language English
 renderRooms('en');
+// === SLIDESHOW SCRIPT ===
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
+
+// Tự động chạy ảnh mỗi 4 giây
+setInterval(() => {
+  plusSlides(1);
+}, 4000);
